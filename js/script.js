1c45de73490ad20aabad5fb5dc467cabb2dda7b0
Nahuel <ovettini@gmail.com>
2024-03-07 10:41:31 -0300

    ahora si

---
+++ js/script.js
@@ -0,0 +1,167 @@
+Boton1 = document.querySelector("#boton1")
+Boton2 = document.querySelector("#boton2")
+Boton3 = document.querySelector("#boton3")
+Boton4 = document.querySelector("#boton4")
+Boton5 = document.querySelector("#boton5")
+Boton6 = document.querySelector("#boton6")
+Boton7 = document.querySelector("#boton7")
+Boton8 = document.querySelector("#boton8")
+// Boton9 = document.querySelector("#tecla_re2")
+// Boton1_sostenido =document.querySelector("#tecla_do_sostenido")
+// Boton_re_sostenido =document.querySelector("#tecla_re_sostenido")
+// Boton_fa_sostenido =document.querySelector("#tecla_fa_sostenido")
+// Boton_sol_sostenido =document.querySelector("#tecla_sol_sostenido")
+// Boton_la_sostenido =document.querySelector("#tecla_la_sostenido")
+// Boton1_sostenido2 =document.querySelector("#tecla_do_sostenido2")
+audio_1 = document.querySelector("#tono1")
+audio_2 = document.querySelector("#tono2")
+audio_3 = document.querySelector("#tono3")
+audio_4 = document.querySelector("#tono4")
+audio_5 = document.querySelector("#tono5")
+audio_6 = document.querySelector("#tono6")
+audio_7 = document.querySelector("#tono7")
+audio_8 = document.querySelector("#tono8")
+// audio_22 = document.querySelector("#tonoRe2")
+// audio_1_sostenido = document.querySelector("#tonoDoSostenido")
+// audio_2_sostenido = document.querySelector("#tonoReSostenido")
+// audio_4_sostenido = document.querySelector("#tonoFaSostenido")
+// audio_5_sostenido = document.querySelector("#tonoSolSostenido")
+// audio_6_sostenido = document.querySelector("#tonoLaSostenido")
+// audio_1_sostenido2 = document.querySelector("#tonoDoSostenido2")
+// fondo= document.querySelector("#fondo")
+
+
+window.onload=function(){
+
+Boton1.addEventListener("click", () => {
+        audio_1.load()
+        audio_1.play()
+   })
+Boton2.addEventListener("click", () => {
+        audio_2.load()
+        audio_2.play()
+})
+Boton3.addEventListener("click", () => {
+        audio_3.load()
+        audio_3.play()
+})
+Boton4.addEventListener("click", () => {
+        audio_4.load()
+        audio_4.play()
+ })
+Boton5.addEventListener("click", () => {
+        audio_5.load()
+        audio_5.play()
+
+})
+Boton6.addEventListener("click", () => {
+        audio_6.load()
+        audio_6.play()
+ 
+})
+Boton7.addEventListener("click", () => {
+        audio_7.load()
+        audio_7.play()
+ 
+})
+Boton8.addEventListener("click", () => {
+        audio_8.load()
+        audio_8.play()
+})}
+// Boton_re2.addEventListener("click", () => {
+//         audio_22.load()
+//         audio_22.play()
+// })
+// Boton1_sostenido.addEventListener("click", () => {
+//         audio_1_sostenido.load()
+//         audio_1_sostenido.play()
+   
+// })
+// Boton_re_sostenido.addEventListener("click", () => {
+//         audio_2_sostenido.load()
+//         audio_2_sostenido.play()
+      
+        
+// })
+// Boton_fa_sostenido.addEventListener("click", () => {
+//         audio_4_sostenido.load()
+//         audio_4_sostenido.play()
+   
+    
+        
+// })
+// Boton_sol_sostenido.addEventListener("click", () => {
+//         audio_5_sostenido.load()
+//         audio_5_sostenido.play()
+    
+        
+// })
+// Boton_la_sostenido.addEventListener("click", () => {
+//         audio_6_sostenido.load()
+//         audio_6_sostenido.play()
+   
+        
+// })
+// Boton1_sostenido2.addEventListener("click", () => {
+//         audio_1_sostenido2.load()
+//         audio_1_sostenido2.play()
+   
+// })
+// Boton1.addEventListener("click", () => {
+//     fondo.className ='do'
+
+// });
+// Boton_re.addEventListener("click", () => {
+//     fondo.className ='re';
+// });
+// Boton_mi.addEventListener("click", () => {
+//     fondo.className ='mi';
+
+// });
+// Boton_fa.addEventListener("click", () => {
+//     fondo.className ='fan ';
+
+// });
+// Boton_sol.addEventListener("click", () => {
+//     fondo.className ='sol'
+
+// });
+// Boton_la.addEventListener("click", () => {
+//     fondo.className ='la'
+
+// });
+// Boton_si.addEventListener("click", () => {
+//     fondo.className ='si'
+
+// });
+// Boton12.addEventListener("click", () => {
+//     fondo.className ='do2';
+
+// });
+// Boton_re2.addEventListener("click", () => {
+//     fondo.className ='re2';
+
+// });
+// Boton1_sostenido.addEventListener("click", () => {
+//     fondo.className ='dos';
+// });
+// Boton_re_sostenido.addEventListener("click", () => {
+//     fondo.className ='res'
+
+// });
+// Boton_fa_sostenido.addEventListener("click", () => {
+//     fondo.className ='fas';
+
+// });Boton_sol_sostenido.addEventListener("click", () => {
+//     fondo.className ='sols'
+
+// });
+// Boton_la_sostenido.addEventListener("click", () => {
+//     fondo.className ='las';
+
+// });
+// Boton1_sostenido2.addEventListener("click", () => {
+//     fondo.className ='dos2';
+
+// });
+// }
